5f0c73132110a2e6da947f5c843c0ca8b5e2555c
CHIMEZIE ASOUZU <shegy28@gmail.com>
2023-04-05 21:48:58 +0200

    Update model to default counters to "0" if nil

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
 
+  def initialize(attributes = {})
+    super
+    self.posts_counter ||= 0
+  end
+
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
