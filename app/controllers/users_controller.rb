a2ed9d2bad3d5a4ed0d8714b1e30695c8fbf4a47
CHIMEZIE ASOUZU <shegy28@gmail.com>
2023-04-10 16:17:47 +0200

    Fix N + 1 issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all.order(created_at: :asc)
+    @users = User.all.order(created_at: :asc).includes(:posts)
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.includes(posts: [:likes, :comments]).find(params[:id])
   end
 end
