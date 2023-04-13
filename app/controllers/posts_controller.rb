38b2084d4cd2d2c360cff7fce3f69a82f203bce4
George kinyanjui <kinyanjuigeorge827@gmail.com>
2023-04-13 10:41:10 +0300

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,7 +28,12 @@ class PostsController < ApplicationController
     flash[:success] = 'Post created!'
   end
 
-  
+  def destroy
+    @post = current_user.posts.find(params[:id])
+    @post.destroy
+    flash[:success] = 'Post deleted!'
+    redirect_to user_posts_path
+  end
 
   private
 
